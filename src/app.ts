181b86bfaeebcba3dca60fa7f7fde30901afa31b
rsjhon90 <rsjhony@hotmail.com>
2021-07-28 20:53:46 -0300

    First implementation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,30 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import router from './routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 export default app;
